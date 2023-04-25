e5c5afee71b2d42a67a27d6d00cc52d7e92e64ba
omar <omar.a.farouk22@gmail.com>
2023-04-25 15:40:27 -0700

    UPDATED

---
+++ _put.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _puts - return the length of a string and print it to stdout
+ * @str: input string
+ *
+ * Return: nothing
+ */
+
+int _puts(char *str)
+{
+	char *ch = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+ * _putchar - print each character send to it by _puts to stdout
+ *
+ * @c: character to be print
+ * Return: 1 on success, -1 on failure
+ */
+
+int _putchar(int c)
+{
+	static int i;
+	static char buffer[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buffer, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buffer[i++] = c;
+	return (1);
+}
