a8123fb2a4d4b889e46d0b9d4bc464d74505dbb1
omar <omar.a.farouk22@gmail.com>
2023-04-25 17:23:29 -0700

    UPDATED

--- specifier.c
+++ specifier.c
@@ -45,7 +45,7 @@ return (NULL);
 int get_print_func(char *s, va_list ap, params_t *params)
 {
 
-int (*f)(va_list, params_t) = get_specifier(s);
+int (*f)(va_list, params_t *) = get_specifier(s);
 
 if (f)
 return (f(ap, params));
